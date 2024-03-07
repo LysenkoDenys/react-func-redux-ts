7accc7aed637c45e6a85bfdbfc6ebf658256d79b
Lysenko <lysenkoden@gmail.com>
2024-03-07 09:21:15 +0100

    fix name of index file and wired up react with redux and ts

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,9 +1,14 @@
 import { useState } from 'react';
 import { useActions } from '../hooks/useActions';
+import { useTypedSelector } from '../hooks/useTypedSelector';
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('');
   const { searchRepositories } = useActions();
+
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.repositories
+  );
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -20,6 +25,9 @@ const RepositoriesList: React.FC = () => {
         />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>loading...</h3>}
+      {!loading && !error && data.map((el) => <div key={el}>{el}</div>)}
     </div>
   );
 };
